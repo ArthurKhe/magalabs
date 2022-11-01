99e44a31be7ff463ad032b60417155580988f20d
arturkhe <artur.khe@osinit.com>
2022-11-01 15:12:13 +0300

    fifth task fix

--- fifth.py
+++ fifth.py
@@ -5,15 +5,12 @@ master = tkinter.Tk()
 canvas = tkinter.Canvas(master, bg='white', height=600, width=600)
 canvas.pack()
 
-r = 100
+r = int(input('Размер:'))
 
-n = int(input('Количество лучей:'))
+n = 6
 
 for k in range(n):
-    if n % 2 == 0:
-        k1 = (k + n / 2 - 1) % n
-    else:
-        k1 = (k + (n - 1) / 2) % n
+    k1 = (k + n / 2 - 1) % n
 
     p1 = (300 + r * math.cos(2 * 3.14 * k / n), 300 + r * math.sin(2 * 3.14 * k / n))
     p2 = (300 + r * math.cos(2 * 3.14 * k1 / n), 300 + r * math.sin(2 * 3.14 * k1 / n))
